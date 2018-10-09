fd3228ab9dcbb4b36c2cb3e5fd1b4dab062ca93d
Richard Smiley <rsmiley@gmail.com>
2018-10-09 00:09:34 -0400

    new format

--- copy.sh
+++ copy.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 rsync -arh --progress /media/smiley/54FF-1FEE/ device/
-find . -name '*.aif' -print -exec zip '{}'.zip '{}' \; -exec rm '{}' \;
+find . -name '*.aif' -print -exec touch -a -m -t 201512180130.09 '{}' \;
+find device/album -name '*.aif' -print -exec zip '{}'.zip '{}' \; -exec rm '{}' \;
+find device/tape -name '*.aif' -print -exec zip '{}'.zip '{}' \; -exec rm '{}' \;
